098e791514c8c336eba21f2e724571111635e9c0
unknown <ikmssaid24>
2025-10-28 14:08:38 +0100

    restaurando

--- main.py
+++ main.py
@@ -1,45 +1,88 @@
-from types import SimpleNamespace
-import odooApi, json
+import json, xmlrpc.client
 from flask import Flask, jsonify, request
 
+url = "http://odoo-grupo3.duckdns.org"
+DB = "edu-Tech-Solutions"
+USR = ""
+PASS=""
+common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+UID= ""
 
 app = Flask(__name__)
-usuarios=[]
 
 @app.route("/login", methods=['POST'])
 
 def login():
-    data =request.get_json()
-    if odooApi.login(data[0], data[1]):
-        return jsonify({"Exito": "Autentificacion realizada exitosamente"}), 200
-    else:
-        return jsonify({"mensaje": "Recurso no encontrado"}), 404
-
-@app.route("/get", methods=['GET'])
-
-def hello_world():
-    
-    odooApi.buscarId(tabla='res.partner',filtro=f)
-    return "exito"
-
-@app.route("/add", methods=['POST'])
-
-def addUsuarios():
     data= request.get_json()
+    global USR, PASS, UID
+    USR=data.get("user")
+    PASS=data.get("password")
+    UID = common.authenticate(DB, USR, PASS, {})
+    print(UID)
+    if UID:
+        return jsonify("Autenticacion completada con exito..."), 200
+    else:
+        return jsonify("Error, la autenticacion ha fallado"), 404
 
-    for i in data:
-        nombre= i.get("nombre", "Desconocido")
-        edad= i.get("edad")
-        usuarios.append({"nombre": nombre, "edad": edad})
-    return jsonify({"mensaje":f"hola, {nombre}", "registros":usuarios})
+@app.route("/searchID", methods=['GET'])
+def getID():
+    data = request.get_json()
+    tabla = data.get("tabla")
+    return models.execute_kw(DB, UID, PASS, tabla, 'search', [[]]), 200
 
-@app.route("/delete/<int:id>", methods=['DELETE'] )
+@app.route("/getCantidad", methods=['GET'])
+def getCantidad():
+    data = request.get_json()
+    tabla = data.get("tabla")
+    return models.execute_kw(DB, UID, PASS, tabla, 'search_count', [[]])
 
+@app.route("/getDatosID", methods=['GET'])
+def getDatos(id):
+    data = request.get_json()
+    tabla = data.get("tabla")
+    id = data.get("id")
+    return models.execute_kw(DB, UID, PASS, tabla, 'read', [[id]]), 200
+
+
+
+@app.route("/getDatosFiltro", methods=['GET'])
+def getFiltrosYCampos():
+    data = request.get_json()
+    tabla = data.get("tabla")
+    campos = data.get("campos")
+    filtros = data.get("filtros")
+    return models.execute_kw(DB, UID, PASS, tabla, 'search_read', [[filtros]], {'fields': campos}), 200
+
+
+
+@app.route("/nuevo", methods=['POST'])
+def nuevo():
+    data= request.get_json()
+    tabla = data.get("tabla")
+    nuevo = data.get("nuevo")
+    return jsonify(models.execute_kw(DB, UID, PASS, tabla, 'create', [nuevo])), 
+
+@app.route("/modificar", methods=['PUT'])
+def modificar():
+    data= request.get_json()
+    tabla = data.get("tabla")
+    id = data.get("id")
+    if models.execute_kw(DB, UID, PASS, tabla, 'write', [[id], data]):
+        return "Operacion extiosa", 200
+    else:
+        return "Oops ha habido un error...", 404
+    
+
+@app.route("/eliminar/<int:id>", methods=['DELETE'])
 def eliminar(id):
-    try:
-        return jsonify(usuarios.pop(id)), 200
-    except IndexError:
-        return jsonify({"mensaje": "Recurso no encontrado"}), 404
+    data= request.get_json()
+    tabla = data.get("tabla")
+    tabla = data.get("id")
+    models.execute_kw(DB, UID, PASS, 'res.partner', 'unlink', [[id]])
+    return jsonify("Usuario eliminado exitosamente"), 200
+
+
 
 if __name__=="__main__":
     app.run(debug=True, port=5000)
